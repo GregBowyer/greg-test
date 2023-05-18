a39114ecdc31cd32601b8a51fca53df64cc4b41f
Greg Bowyer <gbowyer@fastmail.co.uk>
2023-05-18 16:31:36 -0700

    Update default.nix

--- pkgs/example-package/default.nix
+++ pkgs/example-package/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "example-package-${version}";
-  version = "1.0";
+  version = "1.1";
   src = ./.;
   buildPhase = "echo echo Hello World > example";
   installPhase = "install -Dm755 example $out";
